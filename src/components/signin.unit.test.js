31f16e3746dde9a55e08c049fcca7d0bc210dd10
valentin.hetriuc <valentin.hetriuc@gmail.com>
2023-01-26 20:15:37 +0200

    feat: integration with testing lib

---
+++ src/components/signin.unit.test.js
@@ -0,0 +1,10 @@
+import { describe } from "riteway";
+
+describe("sign in component", async (assert) => {
+  assert({
+    given: "no props",
+    should: "render nothing",
+    expected: 0,
+    actual: 1,
+  });
+});
