065ed47dc3e28f8f3ffaf46cdd0285c63ce6897a
valentin.hetriuc <valentin.hetriuc@gmail.com>
2023-01-26 19:47:34 +0200

    feat: add RITE lib

---
+++ src/components/signIn.js
@@ -0,0 +1,8 @@
+const SignIn = () => (
+  <form method="POST">
+    <label for="email">email</label>
+    <input type="text" id="email" name="email" />
+    <button>Sign in</button>
+  </form>
+);
+export default SignIn;
